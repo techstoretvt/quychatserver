0769803493260692799f8f0a3f1998970a459a0e
techstoretvt <techstoretvt2565@gmail.com>
2024-07-26 11:12:53 +0700

    dfhdfgh

---
+++ src/sequelize/models/user.ts
@@ -0,0 +1,25 @@
+/* eslint-disable prettier/prettier */
+/* eslint-disable @typescript-eslint/no-var-requires */
+const { Sequelize, DataTypes } = require('sequelize');
+const sequelize = new Sequelize('sqlite::memory:');
+
+const User = sequelize.define(
+    'User',
+    {
+        // Model attributes are defined here
+        firstName: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        lastName: {
+            type: DataTypes.STRING,
+            // allowNull defaults to true
+        },
+    },
+    {
+        // Other model options go here
+    },
+);
+
+// `sequelize.define` also returns the model
+console.log(User === sequelize.models.User); // true
